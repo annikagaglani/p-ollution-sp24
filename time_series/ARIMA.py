0e688ca97c585de421658673440f9959eabecdad
Brecken Enright <benright5114@scrippscollege.edu>
2024-04-19 12:01:17 -0700

    dates fixed in arima

--- time_series/ARIMA.py
+++ time_series/ARIMA.py
@@ -1,12 +1,12 @@
 from pandas import read_csv
-import datetime
+from datetime import datetime
 from matplotlib import pyplot
 
 def parser(x):
-    return datetime.strptime()
+    return datetime.strptime(x, '%m/%d/%Y')
 
 def initialPlot(input_file):
-    series = read_csv(input_file)
+    series = read_csv(input_file, header=0, parse_dates=[0], index_col=0, date_parser=parser)
     print(series.head())
     series.plot()
     pyplot.show()
