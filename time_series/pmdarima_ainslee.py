868d112699707fa2d4fcdbd07f7ec22927d6aca9
ains-arch <ainsleearchibald@gmail.com>
2024-04-28 21:36:38 -0700

    first try at the forecasts

--- time_series/pmdarima_ainslee.py
+++ time_series/pmdarima_ainslee.py
@@ -4,12 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pandas import read_csv, DataFrame
 from datetime import datetime
-from sklearn.externals import joblib
+import joblib
 
 print("hi idiot")
 
 # Read in file and parse dates
-input_file = "PM25_ARIMA.csv"
+input_file = "monthly_pm25.csv"
 def parser(x):
     return datetime.strptime(x, '%m/%d/%Y')
 pm_series = read_csv(input_file, header=0)
@@ -17,13 +17,13 @@ print("read done")
 
 # Split data
 train_size = int(len(pm_series) * 0.66)
-pm_X = pm_series[['date']]
-pm_y = pm_series['AQI']
+pm_X = pm_series[['Year']]
+pm_y = pm_series['Average_AQI']
 X_train, X_test, y_train, y_test = train_test_split(pm_X, pm_y, test_size=0.33)
 print("train test split done")
 
 # Fit model
-model = pm.auto_arima(y_train, seasonal=True, m=365)
+model = pm.auto_arima(y_train, seasonal=True, m=12)
 print("model fit done")
 
 # Save model
@@ -40,7 +40,7 @@ print("forecasts saved")
 
 # Visualize the forecasts (blue=train, green=forecasts)
 print("visualizing...")
-x = np.arange(series.shape[0])
-plt.plot(x[:train_size], y_train, c='blue')
-plt.plot(x[train_size:], forecasts, c='green')
-plt.show()
+x = np.arange(pm_y.shape[0])
+plt.plot(x[:len(y_train)], y_train, c='blue')
+plt.plot(x[len(y_train):], forecasts, c='green')
+plt.savefig('monthly_pm25_forecasts.png')
